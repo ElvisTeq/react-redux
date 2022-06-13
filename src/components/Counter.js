42f3a58771df8894194178a9400d789113cb4cfb
elvis <cenelvis@gmail.com>
2022-06-13 13:26:55 -0400

    Redux with Class-Based Components, { connect } from react-redux, {Component} from react

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -29,3 +29,59 @@ const Counter = () => {
 };
 
 export default Counter;
+
+/*
+
+// ** 5 Redux with Class-Based Components **
+
+ import { Component } from "react";
+ import { connect } from "react-redux";
+
+class Counter extends Component {
+  incrementHandler() {
+    this.props.increment();
+  }
+
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+// Equivalent to => useSelector
+const mapStateToProps = (state) => {
+  return {
+    // This will be passed as "props" to the class
+    counter: state.counter,
+  };
+};
+
+// Equivalent to => useDispatch
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // This will be passed as "props" to the class
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+  };
+};
+
+// connect => Receives 2 functions
+// returns a function => which we call our class (Counter) as parameter
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+*/
